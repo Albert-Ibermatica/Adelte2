5228786a4e9d03d5d7b8a6856d1a65c54b4a3ae7
extagonzalezd <a.gonzalez.diaz@ibermatica.com>
2023-02-21 17:08:15 +0100

    aaaaa

--- static/js/manage_servers.js
+++ static/js/manage_servers.js
@@ -1,12 +1,3 @@
-const delay = ms => new Promise(res => setTimeout(res, ms));
-const cargandoModelo = async () => {
-    await delay(40000);
-
-    var elem = document.getElementById('servidorActivo')
-    elem.innerText = "Servidor AWS: Cargando modelo TF ..."
-    elem.style.color ="blue"
-
-};
 
 function stop_instances(){
     socket.connect()
